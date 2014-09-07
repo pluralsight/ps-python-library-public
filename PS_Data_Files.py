5f823fc8ff4e9fade97b32ca91758ba81cb91ac8
dustin-vannoy <dustin-vannoy@localhost.localdomain>
2014-09-07 10:18:31 -0700

    Added initial public modules used for processing data into files and loading to BigQuery.

---
+++ PS_Data_Files.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+import logging
+logging.basicConfig() #included to avoid message when oauth2client tries to write to log
+
+
+def loopCSVFile(sourceCSV):
+    import csv
+    fileData = []
+    with open(sourceCSV,'rb') as csvfile:
+        fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in fileReader:
+            fileData.append(row)
+    return fileData
+
+
+def loopDelimitedFile(sourceFile,delimiter=',',quotechar='"'):
+    import csv
+    fileData = []
+    with open(sourceFile,'rb') as csvfile:
+        fileReader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
+        for row in fileReader:
+            fileData.append(row)
+    return fileData
+
+
+def gzipFileCreate(sourceFile,destFile = None):
+    #Create gzip file from existing file (https://docs.python.org/2/library/gzip.html)
+    import gzip
+    #destCSV = sourceCSV + '.gz'
+    if destFile is None:
+        destFile = sourceFile + '.gz'
+    f_in = open(sourceFile, 'rb')
+    f_out = gzip.open(destFile,'wb')
+    f_out.writelines(f_in)
+    f_out.close()
+    f_in.close()
+
