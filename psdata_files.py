30604860a213aaa21cff6e691789456512bee63e
James-Mission <james-mission@pluralsight.com>
2015-02-11 15:22:11 -0800

    Update psdata_files.py
    changed bug that had sourcecsv instead of source_csv

--- psdata_files.py
+++ psdata_files.py
@@ -13,7 +13,7 @@ def loop_csv_file(source_csv):
     """Loops through a comma separated file and returns a list of data.  If column names are on the
     first line of the file they will be the first row of the list
 
-    Args: 
+    Args:
         source_csv: source path for the file
 
     Results:
@@ -21,7 +21,7 @@ def loop_csv_file(source_csv):
     """
     import csv
     file_data = []
-    with open(sourcecsv,'rb') as csvfile:
+    with open(source_csv,'rb') as csvfile:
         file_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in file_reader:
             file_data.append(row)
@@ -32,7 +32,7 @@ def loop_delimited_file(source_file,delimiter=',',quotechar='"'):
     """Loops through a delimited file and returns a list of data.  If column names are on the
     first line of the file they will be the first row of the list
 
-    Args: 
+    Args:
         source_file: source path for the file
         delimiter: character/string to use as file delimited (default delimiter is comma)
         quotechar: character used to qualify strings (default is '"')
@@ -52,11 +52,11 @@ def loop_delimited_file(source_file,delimiter=',',quotechar='"'):
 def gzip_file_create(source_file,dest_file = None):
     """Create gzip file from existing file (https://docs.python.org/2/library/gzip.html).
     Note: this does not delete the source file.
-    
+
     Args:
         source_file: source path for the file to zip
         dest_file: path for the zipped version of file (default is source_file + .gz)
-    
+
     Returns:
         None
     """
@@ -74,11 +74,11 @@ def gzip_file_create(source_file,dest_file = None):
 def gzip_decompress(source_file,dest_file = None):
     """Create unzipped file from existing gzip file (https://docs.python.org/2/library/gzip.html).
     Note: this does not delete the source file.
-    
+
     Args:
         source_file: source path for the file to unzip
         dest_file: path to write the unzipped version of file (default is source_file - .gz)
-    
+
     Returns:
         None
     """
@@ -98,7 +98,7 @@ def get_schema_file(schema_csv):
 
     Args:
         schema_csv: two column comma delimited schema file formatted ColumnName,Type
-    
+
     Returns:
         list, Schema list of column and type
     """
@@ -110,14 +110,20 @@ def get_schema_file(schema_csv):
 
 
 def json_file_to_tsv(source_file, dest_file, fieldnames, source_gzipped=False, boolean_type_supported=False, include_header_row=True):
-    """Read JSON data and write out a tab separated file, typically case is to bulk load it to SQL Server
+    """Read JSON data and write out a tab separated file, typically case is to bulk load it to SQL
+Server
         Args:
             source_file: full path of json file to change to tab delimited
-            dest_file: full path for destination file (file extension is not added by this function)
-            fieldnames: list of field names that match dictionary keys, such as fieldnames = ['column1', 'column2', 'column3']
-            sourect_gzipped: boolean, optional way to specify source file needed unzipped (default is False)
-            boolean_type_supported: boolean, created to override conversion of boolean values to bit for loading to SQL Server,
-                set to True if True/False entries should not be converted to 1 or 0 (default is False)
+            dest_file: full path for destination file (file extension is not added by this function
+)
+            fieldnames: list of field names that match dictionary keys, such as fieldnames = ['colu
+mn1', 'column2', 'column3']
+            sourect_gzipped: boolean, optional way to specify source file needed unzipped (default
+is False)
+            boolean_type_supported: boolean, created to override conversion of boolean values to bi
+t for loading to SQL Server,
+                set to True if True/False entries should not be converted to 1 or 0 (default is Fal
+se)
         Returns:
             None
     """
@@ -128,7 +134,7 @@ def json_file_to_tsv(source_file, dest_file, fieldnames, source_gzipped=False, b
 
     # Build list of dictionaries (one list record per data row)
     data_records_list = []
-    with open(source_file, 'rt') as file_in:
+    with open(source_file, 'rb') as file_in:
         for row in file_in:
             tmp_dct = json.loads(row)
             for k, z in tmp_dct.items():
@@ -148,10 +154,11 @@ def json_file_to_tsv(source_file, dest_file, fieldnames, source_gzipped=False, b
 
     # Write list of dictionaries to tab separated file
     out_file_path = dest_file
-    out_file = open(out_file_path, 'wt')
+    out_file = open(out_file_path, 'wb')
     csvwriter = csv.DictWriter(out_file, delimiter='\t', fieldnames=fieldnames, dialect='excel')
     if include_header_row:
         csvwriter.writerow(dict((fn,fn) for fn in fieldnames))
     for row in data_records_list:
-         csvwriter.writerow(row)
+        del row['CourseTags']
+        csvwriter.writerow(row)
     out_file.close()
